d5bed0e98e6c12272d47403c9562a6adc6eb1bf8
Mateo Marin <mateomarin977@gmail.com>
2021-05-10 10:33:13 +0200

    Updated documentation

--- index.js
+++ index.js
@@ -51,6 +51,34 @@ app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
 
+app.get("/movies:title", (req, res) => {
+  res.send("Successful GET request returning movie title");
+});
+
+app.get("/directors", (req, res) => {
+  res.send("Successful GET request returning list of directors");
+});
+
+app.post("/users", (req, res) => {
+  res.send("Successful POST request for user register");
+});
+
+app.delete("/users", (req, res) => {
+  res.send("Successful DELETE request for user unregister");
+});
+
+app.get("/users/:username/favorites", (req, res) => {
+  res.send("Successful GET request returning favorite movies");
+});
+
+app.delete("/users/:username/favorites/:movie", (req, res) => {
+  res.send("Successful DELETE request for favorite movies");
+});
+
+app.post("/login", (req, res) => {
+  res.send("Successful POST request for user login");
+});
+
 // Error Handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
